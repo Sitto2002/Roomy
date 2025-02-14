f7e1cac6fbd2336a1b6cd8d46d825e87f4c794dd
Shreyy <106059820+Sitto2002@users.noreply.github.com>
2025-02-14 22:22:00 +0530

    Update Value.jsx

--- client/src/components/Value/Value.jsx
+++ client/src/components/Value/Value.jsx
@@ -34,7 +34,7 @@ const Value = () => {
           <span className="primaryText">Value We Give to You</span>
 
           <span className="secondaryText">
-            We always ready to help by providijng the best services for you.
+            We always ready to help by providing the best services for you.
             <br />
             We beleive a good blace to live can make your life better
           </span>
